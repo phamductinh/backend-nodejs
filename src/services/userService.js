9f78782e5c89a12e14338f01b016abdf1b6b5d45
phamductinh18 <sundz.1808@gmail.com>
2022-10-30 23:20:14 +0700

    update

--- src/services/userService.js
+++ src/services/userService.js
@@ -21,7 +21,13 @@ let handleUserLogin = (email, password) => {
 			let isExist = await checkUserEmail(email);
 			if (isExist) {
 				let user = await db.User.findOne({
-					attributes: ["email", "roleId", "password", "firstName", "lastName"],
+					attributes: [
+						"email",
+						"roleId",
+						"password",
+						"firstName",
+						"lastName",
+					],
 					where: { email: email },
 					raw: true,
 				});
@@ -116,8 +122,9 @@ let createNewUser = (data) => {
 				lastName: data.lastName,
 				address: data.address,
 				phonenumber: data.phonenumber,
-				gender: data.gender === "1" ? true : false,
+				gender: data.gender,
 				roleId: data.roleId,
+				positionId: data.positionId,
 			});
 			resolve({
 				errCode: 0,
